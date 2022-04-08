ee1084ef598debeb4af586c7255951b30baf93d5
Alan Tavares <alan_tj@hotmail.com.br>
2022-04-08 14:27:53 -0300

    delete tarefa

--- crud/tarefas/views.py
+++ crud/tarefas/views.py
@@ -4,3 +4,7 @@ from .models import Tarefa
 def index(request):
     tarefas = Tarefa.objects.all()
     return render(request,"index.html",{'tarefas':tarefas})
+def destroy(request, id):
+    tarefa = Tarefa.objects.get(id=id)
+    tarefa.delete()
+    return render(request,"index.html")
