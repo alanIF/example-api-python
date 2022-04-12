c1519adaa0978cbcf11e5c6c928ee131e3ec9710
Alan Tavares <alan_tj@hotmail.com.br>
2022-04-12 16:08:17 -0300

    urls

---
+++ sigprodutos/produtos/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('/new', views.new, name='new'),
+    path('/store/', views.store, name='store'),
+    path('/delete/<int:id>/', views.delete, name='delete'),
+    path('/edit/<int:id>/', views.edit, name='edit'),
+    path('/update/', views.update, name='update'),
+
+]
