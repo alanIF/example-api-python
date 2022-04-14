c3dd81cc871edb6c91991b9447fa5362f0df5e1c
Alan Tavares <alan_tj@hotmail.com.br>
2022-04-14 05:49:49 -0300

    list produto and delete

--- sigprodutos/produtos/urls.py
+++ sigprodutos/produtos/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('/new', views.new, name='new'),
-    path('/store/', views.store, name='store'),
-    path('/delete/<int:id>/', views.delete, name='delete'),
-    path('/edit/<int:id>/', views.edit, name='edit'),
-    path('/update/', views.update, name='update'),
+    path('index', views.index, name='index'),
+    path('new', views.new, name='new'),
+    path('store/', views.store, name='store'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('update/', views.update, name='update'),
 
 ]
--- sigprodutos/produtos/views.py
+++ sigprodutos/produtos/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Produto
 
-# Create your views here.
+def index(request):
+    produtos = Produto.objects.all()
+    return render(request,"index.html",{'produtos':produtos})
+def new(request):
+    return render(request,"index.html")
+def store(request):
+    return render(request,"index.html")
+def update(request):
+    return render(request,"index.html")
+def edit(request,id):
+    return render(request,"index.html")
+def delete(request, id):
+    produto = Produto.objects.get(id=id)
+    produto.delete()
+    return render(request,"index.html")
